6bcf73c8ffa2e4be9cc0936fc3aa801530398b11
WTP MacBook <wtpmacbook@MacBook-Pro.local>
2021-06-04 10:05:28 -0400

    v1 custom elementor widget

---
+++ elementor-wtp-addon.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Elementor WTP Addon WordPress Plugin
+ *
+ * @package ElementorWtpAddon
+ *
+ * Plugin Name: Elementor WTP Addon
+ * Description: Simple Elementor plugin example
+ * Plugin URI:  https://webtrafficpartners.com/
+ * Version:     1.0.0
+ * Author:      Alex Darmos
+ * Author URI:  https://webtrafficpartners.com
+ * Text Domain: elementor-wtp-addon
+ */
+
+define( 'Elementor_Wtp_Addon', __FILE__ );
+
+/**
+ * Include the Elementor_Wtp_Addon class.
+ */
+require plugin_dir_path( Elementor_Wtp_Addon ) . 'class-elementor-wtp-addon.php';
